eab0b0f3f342c62c8e2bfcfd837916a5d445712c
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-01-03 11:13:36 +0100

    Add script to convert from pcd to ply in one directory

---
+++ scripts/convert_all_pcd2ply.sh
@@ -0,0 +1,15 @@
+# !/usr/bin/zsh
+# @file      convert_all_pcd2ply.sh
+# @author    Ignacio Vizzo     [ivizzo@uni-bonn.de]
+#
+# Copyright (c) 2019 Ignacio Vizzo, all rights reserved
+
+# For now, let's do it in the current working directory
+PWD=$(pwd)
+echo "\e[1m\e[36mConverting all .pcd scans we find in $PWD ...\e[0m"
+
+for SCAN in $PWD/*.pcd; do
+  [ -f "$SCAN" ] || continue
+  SCAN_NAME="${SCAN%.*}"
+  pcl_pcd2ply $SCAN $SCAN_NAME.ply
+done
