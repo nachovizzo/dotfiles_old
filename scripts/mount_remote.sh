ad883c1ee43bdd0f2004e7ba151e19fd5b54adb7
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-02-08 20:17:47 +0100

    Data location changed

--- scripts/mount_remote.sh
+++ scripts/mount_remote.sh
@@ -4,13 +4,11 @@
 #
 # Copyright (c) 2019 Ignacio Vizzo, all rights reserved
 #
-# NOTE: Rememver to add `dataserver` to your /etc/hosts file
-
 # First umount all the mounted remote file systems, just in case
 fusermount -q -u $DATASETS/remote
 
 # This comes from my remote home directory, it's my own fault if I meto la pata
 echo "\e[1m\e[93mMounting datasets from ~dataserver into $DATASETS/remote/\e[0m"
 sshfs -o IdentityFile=$HOME/.ssh/id_rsa \
-  nacho@dataserver:/home/nacho/data/ \
+  nacho@dataserver:/home/nacho/dev/datasets \
   $DATASETS/remote
