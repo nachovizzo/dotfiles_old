82512aaf1e49660edbdf659bc607cea8ccaa1501
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-03-22 12:53:45 -0300

    Add cool alias

--- .aliases.zsh
+++ .aliases.zsh
@@ -37,5 +37,6 @@ alias rmrf='rm -rf'
 alias vp='cp'
 alias kitti_monitor='htop -p $(pgrep -d, eval_sequence) --sort-key=Command'
 alias mapping_monitor='htop -p $(pgrep -d, -f "python3 ./mapping_pipeline.py") --sort-key=Command'
+alias df='df -h -xsquashfs -xtmpfs -xdevtmpfs'
 
 source $HOME/scripts/aliases.zsh
