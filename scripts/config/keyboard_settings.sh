98b448777803927a672665b654abcd8a787eca9a
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-04-01 02:30:47 -0300

    And still doesn't work

--- scripts/config/keyboard_settings.sh
+++ scripts/config/keyboard_settings.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+# @file      keyboard_settings.sh
+# @author    Ignacio Vizzo     [ivizzo@uni-bonn.de]
+#
+# Copyright (c) 2019 Ignacio Vizzo, all rights reserved
 
 # Use CapsLock as Super key
-setxkbmap -option 'caps:super'
+setxkbmap -option 'caps:super' && notify-send -i keyboard "CapsLock" "Disabled"
 
 # Use CapsLock as Escape key if nothing else is pressed aftwerwards
 xcape -e 'Super_L=Escape' -t 300
-
