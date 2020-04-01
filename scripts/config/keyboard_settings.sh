73afabd810e7f595fee69290439865090fbbaaca
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-04-01 02:38:44 -0300

    Add comment in case this doesn't work

--- scripts/config/keyboard_settings.sh
+++ scripts/config/keyboard_settings.sh
@@ -3,6 +3,7 @@
 # @author    Ignacio Vizzo     [ivizzo@uni-bonn.de]
 #
 # Copyright (c) 2019 Ignacio Vizzo, all rights reserved
+# gsettings set org.gnome.settings-daemon.plugins.keyboard active false
 
 # Use CapsLock as Super key
 setxkbmap -option 'caps:super' && notify-send -i keyboard "CapsLock" "Disabled"
