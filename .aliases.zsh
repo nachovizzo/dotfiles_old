08c02fb6b6962c80a68a0af929a4e71c83c22e0f
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-04-10 12:00:32 -0300

    Add alias

--- .aliases.zsh
+++ .aliases.zsh
@@ -39,6 +39,7 @@ alias kitti_monitor='htop -p $(pgrep -d, eval_sequence) --sort-key=Command'
 alias mapping_monitor='htop -p $(pgrep -d, -f "python3 ./mapping_pipeline.py") --sort-key=Command'
 alias df='df -h -xsquashfs -xtmpfs -xdevtmpfs'
 alias i3config='$EDITOR ~/.config/regolith/i3/config'
+alias codeconfig='$EDITOR ~/.config/Code/User/settings.json'
 alias -g latest='*(om[1])'
 
 source $HOME/scripts/config/aliases.zsh
