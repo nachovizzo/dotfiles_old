eb330f1fef8b82de8b2e6d4254cf8a2c676d1eb0
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-04-14 15:37:55 -0300

    Fix scripts

--- scripts/read_clean_py_code.zsh
+++ scripts/read_clean_py_code.zsh
@@ -5,10 +5,8 @@
 # Copyright (c) 2019 Ignacio Vizzo, University of Bonn.
 source load_colors.zsh
 
-echo "Linking ${BOLD_CYAN}~/.clean_py_code.json${RESET} to ${BOLD_BLUE}.vscode/settings.json${RESET}"
+echo "Copying ${BOLD_CYAN}~/scripts/vscode_settings_templates/.clean_py_code.json ${RESET} to ${BOLD_BLUE}.vscode/settings.json${RESET}"
 mkdir -p .vscode/
-if [ -f .vscode/settings.json ]; then
-  mv .vscode/settings.json .vscode/settings.json.bak
-fi
-ln -s vscode_settings_templates/.clean_py_code.json .vscode/settings.json
+cat .vscode/settings.json >/dev/null 2>&1 && mv .vscode/settings.json .vscode/settings.json.bak
+cp $HOME/scripts/vscode_settings_templates/.clean_py_code.json .vscode/settings.json
 echo "done"
