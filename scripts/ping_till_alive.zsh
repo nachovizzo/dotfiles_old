a12546fe058df65acbaa1ced8055783e3be7cb86
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-04-24 13:46:34 -0300

    Add ping till alive script

---
+++ scripts/ping_till_alive.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+# @file      ping_till_alive.zsh
+# @author    Ignacio Vizzo     [ivizzo@uni-bonn.de]
+#
+# Copyright (c) 2020 Ignacio Vizzo, all rights reserved
+source load_colors.zsh
+
+HOST="$1"
+printf "%s" "${BOLD_CYAN}ping ${BOLD_YELLOW}${HOST}${BOLD_CYAN} until it becoms alive ${RESET}"
+while ! timeout 1 ping -c 1 -n $HOST &>/dev/null; do
+  printf "%c" "."
+done
+printf "\n%s\n" "${BOLD_YELLOW}${HOST}${BOLD_CYAN} is online"
+notify-send "${HOST} is online"
