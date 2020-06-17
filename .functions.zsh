aa22c3dabbab6c2ce18c42296af6d5bf9c31e293
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-06-17 19:16:31 -0300

    Pass extra args

--- .functions.zsh
+++ .functions.zsh
@@ -4,9 +4,10 @@ get_remote() {
 
 # Please turn this into a python script
 gdb_test() {
-    TEST_CASE=$@
+    TEST_CASE="$1"
+    EXTRA_ARGS="${@:2}"
     TEST_COMMAND=" $(ctest -V -N -R ${TEST_CASE} | grep -oP "(Test command:\s)\K(.*$)+" | tr -d '"')"
     CWD="$(ctest -R ${TEST_CASE} --show-only=json-v1 | jq '.tests[0].properties[0].value')"
-    eval "gdb -cd="${CWD}" -ex=r --args ${TEST_COMMAND}"
+    eval "gdb -cd="${CWD}" -ex=r --args ${TEST_COMMAND} ${EXTRA_ARGS}"
 }
 
