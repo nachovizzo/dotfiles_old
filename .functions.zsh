b49943ab7cddb2b22b66cee74b63f11c6034b56b
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-06-17 19:09:17 -0300

    Add magic function

--- .functions.zsh
+++ .functions.zsh
@@ -2,3 +2,11 @@ get_remote() {
     git remote -vv | tail -n1 | awk '{print $2}' | tr -d '\n' | xcopy
 }
 
+# Please turn this into a python script
+gdb_test() {
+    TEST_CASE=$@
+    TEST_COMMAND=" $(ctest -V -N -R ${TEST_CASE} | grep -oP "(Test command:\s)\K(.*$)+" | tr -d '"')"
+    CWD="$(ctest -R ${TEST_CASE} --show-only=json-v1 | jq '.tests[0].properties[0].value')"
+    eval "gdb -cd="${CWD}" -ex=r --args ${TEST_COMMAND}"
+}
+
