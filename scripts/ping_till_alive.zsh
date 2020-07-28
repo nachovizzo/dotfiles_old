b902bfdc1a3cf7f4e57cb742a87b166df3b79f68
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-07-28 12:37:50 -0300

    Add sleep time

--- scripts/ping_till_alive.zsh
+++ scripts/ping_till_alive.zsh
@@ -9,6 +9,7 @@ HOST="$1"
 printf "%s" "${BOLD_CYAN}ping ${BOLD_YELLOW}${HOST}${BOLD_CYAN} until it becoms alive ${RESET}"
 while ! timeout 1 ping -c 1 -n $HOST &>/dev/null; do
   printf "%c" "."
+  sleep 1
 done
 printf "\n%s\n" "${BOLD_YELLOW}${HOST}${BOLD_CYAN} is online"
 notify-send "${HOST} is online"
