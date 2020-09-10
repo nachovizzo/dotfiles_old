050a4466592ce17127e980c48b26028d797963d5
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-09-10 13:38:49 +0200

    update

--- scripts/inspect_geometry.py
+++ scripts/inspect_geometry.py
@@ -17,9 +17,11 @@ def main(filename):
     if is_mesh(filename):
         print("Reding TriangleMesh object:", filename)
         mesh = o3d.io.read_triangle_mesh(filename, print_progress=True)
+        print(mesh)
     else:
         print("Reding PointCloud object:", filename)
         cloud = o3d.io.read_point_cloud(filename, print_progress=True)
+        print(cloud)
 
     print("Launching debugging session")
     ipdb.set_trace()
