a4cbc9382c57122a0bef7943dda3042a8b489477
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-10-23 13:34:39 +0200

    more scripts

---
+++ scripts/eval_kitti_all.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+# @file      eval_kitti_all.zsh
+# @author    Ignacio Vizzo     [ivizzo@uni-bonn.de]
+#
+# Copyright (c) 2020 Ignacio Vizzo, all rights reserved
+source load_colors.zsh
+
+echo "Evaluating full KITTI Odometry benchmark in: ${BOLD_CYAN}$(pwd)${RESET}"
+echo "---------------------------------------------------------------------------------------------"
+
+for seq in $(seq -w 00 09); do
+    echo "Sequence ${BOLD_YELLOW}${seq}${RESET}"
+    kitti_seq_err $DATASETS/kitti-odometry/dataset/poses/${seq}.txt *${seq}*txt | tail -n2
+    echo "---------------------------------------------------------------------------------------------"
+done
