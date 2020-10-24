14eebae322602661b4959c7aa0558f021620e30f
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-10-24 09:41:29 +0200

    scripts

--- scripts/eval_kitti_all.zsh
+++ scripts/eval_kitti_all.zsh
@@ -8,7 +8,7 @@ source load_colors.zsh
 echo "Evaluating full KITTI Odometry benchmark in: ${BOLD_CYAN}$(pwd)${RESET}"
 echo "---------------------------------------------------------------------------------------------"
 
-for seq in $(seq -w 00 09); do
+for seq in $(seq -w 00 10); do
     echo "Sequence ${BOLD_YELLOW}${seq}${RESET}"
     kitti_seq_err $DATASETS/kitti-odometry/dataset/poses/${seq}.txt *${seq}*txt | tail -n2
     echo "---------------------------------------------------------------------------------------------"
--- scripts/eval_kitti_seq.zsh
+++ scripts/eval_kitti_seq.zsh
@@ -4,13 +4,12 @@
 #
 # Copyright (c) 2020 Ignacio Vizzo, all rights reserved
 source load_colors.zsh
-source kitti_gt.zsh
 SEQ="$@"
 
 echo "Obtaining KITTI Odometry benchmark metrics for ${BOLD_CYAN}sequence:${SEQ}${RESET} in $(pwd)"
 echo "---------------------------------------------------------------------------------------------"
 for exp in *${SEQ}*.txt; do
     echo "${BOLD_YELLOW}${exp}${RESET}"
-    kitti_seq_err $KITTI_GT_07 $exp | tail -n2
+    kitti_seq_err $DATASETS/kitti-odometry/dataset/poses/${SEQ}.txt $exp | tail -n2
     echo "---------------------------------------------------------------------------------------------"
 done
