1b761304159328c975ab5a6ca11e362ad2bce964
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-11-17 12:23:41 +0100

    Mount desktop data

--- scripts/mount_remote.zsh
+++ scripts/mount_remote.zsh
@@ -2,15 +2,16 @@
 # @file      mount_remote.sh
 # @author    Ignacio Vizzo     [ivizzo@uni-bonn.de]
 #
-# Copyright (c) 2019 Ignacio Vizzo, all rights reserved
+# Copyright (c) 2020 Ignacio Vizzo, all rights reserved
 #
-# First umount all the mounted remote file systems, just in case
 source load_colors.zsh
+
+# First umount all the mounted remote file systems, just in case
 mkdir -p $DATASETS/remote
 fusermount -q -u $DATASETS/remote
 
 # This comes from my remote home directory, it's my own fault if I meto la pata
-echo "${BOLD_YELLOW}Mounting datasets from ~dataserver into $DATASETS/remote/${RESET}"
-sshfs -o IdentityFile=$HOME/.ssh/id_rsa \
-  nacho@dataserver:/home/nacho/dev/datasets \
+echo "${BOLD_YELLOW}Mounting datasets from ${BOLD_CYAN}ipb24${BOLD_YELLOW} into $DATASETS/remote/${RESET}"
+sshfs -o reconnect,ro,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=$HOME/.ssh/id_rsa \
+  ipb24:$HOME/data/ \
   $DATASETS/remote
