ee5333e144550bd3169df450b32093fee3240c21
Ignacio Vizzo <ignaciovizzo@gmail.com>
2021-03-11 19:17:51 +0100

    Add script to merge compile_commands.json

---
+++ scripts/merge_compile_commands.zsh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Cleanup
+rm compile_commands.json 2> /dev/null
+rm tmp.json 2> /dev/null
+
+for json_file in **/*compile_commands.json; do
+    echo "Merging $json_file..."
+    sed -e '1s/^/[\'$'\n''/' -e '$s/,$/\'$'\n'']/' "$json_file" | sed 's/[][]//g' >>tmp.json
+    echo , >>tmp.json
+done
+cat tmp.json | sed '$ s/.$//' >>tmp.json
+
+# Add the fucking square bracktes
+sed -i '1 i\[' tmp.json
+echo ] >>tmp.json
+
+# Format the json file
+python3 -m json.tool tmp.json > compile_commands.json 
+rm tmp.json 2> /dev/null
+
+echo "Success, results ready in ./compile_commands.json"
