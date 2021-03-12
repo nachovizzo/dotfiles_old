1332c4f2ae506289969bd259db5385ea6f8c4c15
Ignacio Vizzo <ignaciovizzo@gmail.com>
2021-03-12 09:30:46 +0100

    Make it magic

--- scripts/merge_compile_commands.zsh
+++ scripts/merge_compile_commands.zsh
@@ -10,17 +10,25 @@ if compgen -G "**/*compile_commands.json" >/dev/null; then
         sed -e '1s/^/[\'$'\n''/' -e '$s/,$/\'$'\n'']/' "$json_file" | sed 's/[][]//g' >>tmp.json
         echo , >>tmp.json
     done
-    cat tmp.json | sed '$ s/.$//' >>tmp.json
+
+    # Remove last ","
+    sed -i '$d' tmp.json
 
     # Add the fucking square bracktes
     sed -i '1 i\[' tmp.json
     echo ] >>tmp.json
 
-    # Format the json file
-    python3 -m json.tool tmp.json >compile_commands.json || echo "Merging Failed" && exit 1
+    # Format the json file, if doesn't succeed, then the JSON file is wrong
+    if python3 -m json.tool tmp.json >compile_commands.json; then
+        echo "Success, results ready in ./compile_commands.json"
+    else
+        echo "Merging failed"
+        rm compile_commands.json 2>/dev/null
+    fi
+
+    # Cleanup
     rm tmp.json 2>/dev/null
 
-    echo "Success, results ready in ./compile_commands.json"
 else
     echo "No compile_commands.json files in $(pwd)"
 fi
