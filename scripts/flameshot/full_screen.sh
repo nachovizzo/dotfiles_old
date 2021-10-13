baf63b5a1a42a727c8f6c8e61edf98c70a2ea2e3
Ignacio Vizzo <ignaciovizzo@gmail.com>
2021-10-13 10:28:37 +0200

    Attempt to fix flameshot

--- scripts/flameshot/full_screen.sh
+++ scripts/flameshot/full_screen.sh
@@ -3,20 +3,24 @@
 # @author    Ignacio Vizzo     [ivizzo@uni-bonn.de]
 #
 # Copyright (c) 2019 Ignacio Vizzo, all rights reserved
-WIDTH=$(xrandr --query | awk -F '[ x,+]' '/connected primary/{print $4}')
-HEIGHT=$(xrandr --query | awk -F '[ x,+]' '/connected primary/{print $5}')
+set -e
+MARGIN=0
+WIDTH=$(expr $(xrandr --query | awk -F '[ x,+]' '/connected primary/{print $4}') - $MARGIN)
+HEIGHT=$(expr $(xrandr --query | awk -F '[ x,+]' '/connected primary/{print $5}') - $MARGIN)
 
 # Launch the snapshot gui
-flameshot gui && sleep 0.05
+flameshot gui &
+sleep 0.1
 
 # Save the current mouse location
 eval $(xdotool getmouselocation --shell)
 
 # Move the mouse to the top left corner and drag it to to the right bottom corner
 xdotool mousemove 0 0
-xdotool mousedown 1 # pres and hold
+xdotool mousedown 1
 xdotool mousemove ${WIDTH} ${HEIGHT}
-xdotool mouseup 1 # release click
+xdotool mousemove 0 0
+xdotool mouseup 1
 
 # Restore mouse to previous location
 xdotool mousemove ${X} ${Y}
