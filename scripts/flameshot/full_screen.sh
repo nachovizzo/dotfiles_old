2cbbd3e85c672e6f8086d722bd48a2c923367307
Ignacio Vizzo <ignaciovizzo@gmail.com>
2021-10-14 16:02:21 +0200

    Simplify script

--- scripts/flameshot/full_screen.sh
+++ scripts/flameshot/full_screen.sh
@@ -1,26 +1,14 @@
 #!/usr/bin/env bash
-# @file      full_screen.sh
-# @author    Ignacio Vizzo     [ivizzo@uni-bonn.de]
-#
-# Copyright (c) 2019 Ignacio Vizzo, all rights reserved
-set -e
-MARGIN=0
-WIDTH=$(expr $(xrandr --query | awk -F '[ x,+]' '/connected primary/{print $4}') - $MARGIN)
-HEIGHT=$(expr $(xrandr --query | awk -F '[ x,+]' '/connected primary/{print $5}') - $MARGIN)
+# Originally https://github.com/flameshot-org/flameshot/issues/273#issuecomment-613660060
+# Taken from https://github.com/flameshot-org/flameshot/issues/523#issuecomment-942058104
+flameshot gui
 
-# Launch the snapshot gui
-flameshot gui &
-sleep 0.2
+while true; do
+    if [ "$(xdotool search --onlyvisible --class flameshot)" = "" ]; then
+        sleep 0.1
+    else
+        break
+    fi
+done
 
-# Save the current mouse location
-eval $(xdotool getmouselocation --shell)
-
-# Move the mouse to the top left corner and drag it to to the right bottom corner
-xdotool mousemove 0 0
-xdotool mousedown 1
-xdotool mousemove ${WIDTH} ${HEIGHT}
-xdotool mousemove 0 0
-xdotool mouseup 1
-
-# Restore mouse to previous location
-xdotool mousemove ${X} ${Y}
+xdotool key ctrl+a
