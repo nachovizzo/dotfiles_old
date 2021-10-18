646ccbb1281d1829aa690fd1ff6eef5b5857709f
Ignacio Vizzo <ignaciovizzo@gmail.com>
2021-10-18 16:18:05 +0200

    Add lua config

---
+++ .vim/config/init.lua
@@ -0,0 +1,46 @@
+-- lualine setup
+require'lualine'.setup {
+  options = {
+    icons_enabled = true,
+    -- theme = 'gruvbox',
+    theme = 'vscode',
+    component_separators = {'', ''},
+    section_separators = {'', ''},
+    disabled_filetypes = {}
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename', 'g:coc_status'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
+
+-- Buffer line setup
+require'bufferline'.setup{
+	options = {
+		indicator_icon = ' ',
+    	buffer_close_icon = '',
+    	modified_icon = '●',
+    	close_icon = '',
+		close_command = "Bdelete %d",
+		right_mouse_command = "Bdelete! %d",
+		left_trunc_marker = '',
+    	right_trunc_marker = '',
+		offsets = {{filetype = "NvimTree", text = "EXPLORER", text_align = "center"}},
+		show_tab_indicators = true,
+		show_close_icon = false
+	},
+}
