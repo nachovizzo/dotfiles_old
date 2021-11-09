ac17b4a1cc064e08548ae3cee70e48011ec255dc
Ignacio Vizzo <ignaciovizzo@gmail.com>
2021-11-09 16:50:59 +0100

    Update flameshot script

--- scripts/flameshot/full_screen.sh
+++ scripts/flameshot/full_screen.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # Originally https://github.com/flameshot-org/flameshot/issues/273#issuecomment-613660060
 # Taken from https://github.com/flameshot-org/flameshot/issues/523#issuecomment-942058104
-flameshot gui
-
-while true; do
-    if [ "$(xdotool search --onlyvisible --class flameshot)" = "" ]; then
-        sleep 0.1
-    else
-        break
-    fi
-done
-
-xdotool key ctrl+a
+# Solved on https://github.com/flameshot-org/flameshot/pull/2013
+# flameshot gui
+# while true; do
+    # if [ "$(xdotool search --onlyvisible --class flameshot)" = "" ]; then
+        # sleep 0.1
+    # else
+        # break
+    # fi
+# done
+# xdotool key ctrl+a
+flameshot gui --region 1920x1080+0+0
