e8329e9e3b0e3434fdb389bcf3f76ae1f58bcb54
Ignacio Vizzo <ignaciovizzo@gmail.com>
2022-02-14 11:26:51 +0100

    Update this crap

--- scripts/kitti_gt.zsh
+++ scripts/kitti_gt.zsh
@@ -3,5 +3,5 @@
 #
 # Copyright (c) 2019 Ignacio Vizzo, all rights reserved
 for SEQ in $(seq -w 0 10); do
-  eval KITTI_GT_${SEQ}='$DATASETS/kitti-odometry/dataset/poses/${SEQ}.txt'
+  eval KITTI_GT_${SEQ}='$DATASETS/kitti-odometry/dataset/kitti-poses/${SEQ}.txt'
 done
