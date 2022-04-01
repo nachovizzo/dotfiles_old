c4b0320957a99669330665a5792f7892f00d0de3
Ignacio Vizzo <ignaciovizzo@gmail.com>
2022-04-01 16:20:52 +0200

    Add stuff

--- scripts/eval_kitti_all_table.zsh
+++ scripts/eval_kitti_all_table.zsh
@@ -10,7 +10,7 @@ echo "Evaluating full KITTI Odometry benchmark in: ${BOLD_YELLOW}$(pwd)${RESET}"
 echo "Translational/Rotational errors:"
 echo -n "trans_error: ["
 for seq in $(seq -w 00 10); do
-    kitti_seq_err $DATASETS/kitti-odometry/dataset/poses/${seq}.txt *${seq}*txt \
+    kitti_seq_err $DATASETS/kitti-odometry/dataset/kitti_poses/${seq}.txt *${seq}*txt \
         | tail -n2 \
         | sed -n '1 p' \
         | grep -E ':(.*)' | grep -Eo '[^:]*$' \
@@ -20,7 +20,7 @@ echo "]"
 
 echo -n "rot_error: ["
 for seq in $(seq -w 00 10); do
-    kitti_seq_err $DATASETS/kitti-odometry/dataset/poses/${seq}.txt *${seq}*txt \
+    kitti_seq_err $DATASETS/kitti-odometry/dataset/kitti_poses/${seq}.txt *${seq}*txt \
         | tail -n1 \
         | grep -E ':(.*)' | grep -Eo '[^:]*$' \
         | tr '\n' ","
