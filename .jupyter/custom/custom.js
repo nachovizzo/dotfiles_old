a64d53974c16da4c253a3859e92726102bb9a103
Ignacio Vizzo <ignaciovizzo@gmail.com>
2022-04-22 15:20:28 +0200

    Remove this for now, I'm not even using it

--- .jupyter/custom/custom.js
+++
@@ -1,7 +0,0 @@
-// Configure CodeMirror Keymap
-require([
-    'nbextensions/vim_binding/vim_binding',   // depends your installation
-], function() {
-    // Map jj to <Esc>
-    CodeMirror.Vim.map("jk", "<Esc>", "insert");
-});
