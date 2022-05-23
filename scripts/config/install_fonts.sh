905e7b90866104c4033949876dd87bdfca42c24e
Ignacio Vizzo <ignaciovizzo@gmail.com>
2022-05-23 10:57:13 +0200

    save

---
+++ scripts/config/install_fonts.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Twixes/SF-Mono-Powerline.git
+cd SF-Mono-Powerline
+mkdir -p ~/.local/share/fonts
+cp *.otf /usr/share/fonts/sfmono
+# docker run -v /path/to/fonts:/in -v /path/for/output:/out nerdfonts/patcher [OPTIONS]
+
